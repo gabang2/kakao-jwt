934d3ebb76d3f80c44f606fbf5467803a24128a0
gabang2 <offbeat1020@naver.com>
2023-09-06 13:55:28 +0900

    로그인 후 KakaoUserInfo Dto로 변환 완료

--- src/main/java/jwt/kakao/oauth/controller/OauthController.java
+++ src/main/java/jwt/kakao/oauth/controller/OauthController.java
@@ -1,8 +1,29 @@
 package jwt.kakao.oauth.controller;
 
+import jwt.kakao.oauth.dto.KakaoUserInfo;
+import jwt.kakao.oauth.service.OauthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@Slf4j
+@RequiredArgsConstructor
 public class OauthController {
+
+    private final OauthService oauthService;
+
+    @ResponseBody
+    @GetMapping("/api/oauth/kakao")
+    public void kakaoCallback(@RequestParam String code) {
+        log.info("code : " + code);
+        String accesstoken = oauthService.getKakaoAccessToken(code);
+        System.out.println(accesstoken);
+        KakaoUserInfo kakaoUserInfo = oauthService.getKakaoUserInfo(accesstoken);
+    }
 
 }
